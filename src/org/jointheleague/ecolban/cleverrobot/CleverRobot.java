f0ce8b78f8c26605c3ad3afc01bd6da7d5d1ea5f
runxue00 <league@WorthyMini-5.attlocal.net>
2017-05-27 17:31:35 -0700

    smile
    I made the roomba turn when it sensed a wall or an object in front of
    it or if it ran into an object.

--- src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
+++ src/org/jointheleague/ecolban/cleverrobot/CleverRobot.java
@@ -10,13 +10,16 @@ import org.jointheleague.ecolban.rpirobot.IRobotAdapter;
 import org.jointheleague.ecolban.rpirobot.IRobotInterface;
 import org.jointheleague.ecolban.rpirobot.SimpleIRobot;
 
+import com.hopding.jrpicam.RPiCamera;
+
 public class CleverRobot extends IRobotAdapter {
 	Sonar sonar = new Sonar();
 	
 	public CleverRobot(IRobotInterface iRobot) {
 		super(iRobot);
 	}
-
+	
+	 RPiCamera piCamera;
 	public static void main(String[] args) throws Exception {
 		System.out.println("Try event listner, rev Monday 2030");
 		IRobotInterface base = new SimpleIRobot();
@@ -29,17 +32,43 @@ public class CleverRobot extends IRobotAdapter {
 
 	private void setup() throws Exception {
 		driveDirect(100,100);
+		piCamera= new RPiCamera("WorthyMini-5.local:");
+		piCamera.takeStill("testing.jpg");
 	}
 	
 	private boolean loop() throws Exception{
-		System.out.println("LEFT SONAR: " + sonar.readSonar("left"));
-		Thread.sleep(1000);
-		System.out.println("RIGHT SONAR: " + sonar.readSonar("right"));
-		System.out.println("CENTER SONAR: " + sonar.readSonar("center"));
-		
-		return true;
-	}
+		readSensors(100);
+		driveDirect(1000, 1000);
+		if(isBumpLeft()){
+			driveDirect(-100, 100);
+			//Thread.sleep(666 - 1337);
+			Thread.sleep(1200);
+		}
+		if(isBumpRight()){
+			driveDirect(-100, 100);
+			//Thread.sleep(666 - 1337);
+			Thread.sleep(1200);
+		}
+		if(isLightBump()){
+			driveDirect(-100, 100);
+			//Thread.sleep(666 - 1337);
+			Thread.sleep(1200);
+		}
 
+		int[]lightvalues = getLightBumps();
+		System.out.println("lightvalues[0]"+lightvalues[0]);
+		System.out.println("lightvalues[1]"+lightvalues[1]);
+		System.out.println("lightvalues[2]"+lightvalues[2]);
+		System.out.println("lightvalues[3]"+lightvalues[3]);
+		System.out.println("lightvalues[4]"+lightvalues[4]);
+		System.out.println("lightvalues[5]"+lightvalues[5]);
+		
+		if(lightvalues.equals("0")){
+		
+		
+		
+	}
+return true;
 	private void shutDown() throws IOException {
 		reset();
 		stop();
